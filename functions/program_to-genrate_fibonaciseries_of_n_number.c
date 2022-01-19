4c75f9b5391f43eb57f42945611c9aa1aec7f6d6
JenilGajjar <93389197+JenilGajjar@users.noreply.github.com>
2022-01-19 19:10:40 +0530

    Update program_to-genrate_fibonaciseries_of_n_number.c

--- functions/program_to-genrate_fibonaciseries_of_n_number.c
+++ functions/program_to-genrate_fibonaciseries_of_n_number.c
@@ -1,1 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+void fibbo(int n)
+{
+    int a = 0, b = 1, sum;
+    printf("%d\t", b);
+    for (int i = 1; i <= n - 1; i++)
+    {
+        sum = a + b;
+        printf("%d\t", sum);
+        a = b;
+        b = sum;
+    }
+    printf("\n");
+}
+void main()
+{
+    int n;
+    printf("Enter Any Number ");
+    scanf("%d", &n);
+    fibbo(n);
+}
