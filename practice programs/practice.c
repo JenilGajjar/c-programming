98535049c38789b661a075bcc1ba0cc8f8166712
JenilGajjar <93389197+JenilGajjar@users.noreply.github.com>
2022-02-16 23:27:38 +0530

    Add files via upload

---
+++ practice programs/practice.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    // {
+    //     for (int n=7; n!=0; n++)
+    //     printf("n=%d",n--);
+    //     getchar();
+    //     return 0;
+    // output of this code is infinite (7n=7n=7n=7n=...)
+    // }
+
+
+    // {
+    //     int x = 10, *y, **z;
+    //     y = &x;
+    //     z = &y;
+    //     printf("%d %d %d", *y, **z, *(*z));
+    // output of this code is 10 10 10  
+    // }
+
+
+    // {
+    // int m,i=0,j=1,k=2;
+    // m= i++ || j++ || k++ ;
+    // printf("%d %d %d %d",m,i,j,k);
+    // output of this code is (1 1 2 2)
+    // }
+
+
+    // {
+    //     int a=1,b=2,c=3;
+    //     char d=0;
+    //     if(a,b,c,d){
+    //         printf("entered in the if \n");
+    //     }
+    //     printf("not entered in if ");
+    //     output not entered in if
+    // }
+}
